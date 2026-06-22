9b93d321387b5e50f32c5ca5eb9eecb93b3664b6
alram1244787338 <1244787338@qq.com>
2026-06-22 11:52:23 +0800

    round 1: 代码生成

---
+++ app.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+router.get('/', async (ctx) => {
+  ctx.body = { message: 'ok' };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
